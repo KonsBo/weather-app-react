7e9cceaf85a4cc3bba96a3fa8acac097470f4712
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-07-06 17:35:11 +0300

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1> Weather App</h1>
-      <footer>
-        This project was coded by Konstantina Boua and is open sourced on{" "}
-        <a
-          href="https://github.com/KonsBo/weather-app-react"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather App</h1>
+        <footer>
+          This project was coded by Konstantina Boua and is open sourced on{" "}
+          <a
+            href="https://github.com/KonsBo/weather-app-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
