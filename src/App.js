877f685868a547d83f8e510d8199d182a8c23514
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-07-10 19:04:30 +0300

    Added Basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
+import React from "react";
 import "./App.css";
+import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App</h1>
+        <Weather defaultCity="New York" />
+
         <footer>
           This project was coded by Konstantina Boua and is open sourced on{" "}
           <a
             href="https://github.com/KonsBo/weather-app-react"
             target="_blank"
-            rel="noreferrer"
+            rel=" noopener noreferrer"
           >
+            {" "}
             GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://react-w3ather-app.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            hosted on Netlify
           </a>
         </footer>
       </div>
     </div>
   );
 }
-
-export default App;
