40b40be86339e4d2c7cdeaa2cd2a29ef079592b4
Konstantina Boua <85695279+KonsBo@users.noreply.github.com>
2023-07-11 18:32:27 +0300

    Update WeatherTemperature.js

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -18,7 +18,10 @@ export default function WeatherTemperature(props) {
       <div className="WeatherTemperature">
         <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
-          °C | <a href="/" onClick={showFahrenheit}></a>
+          °C |{""}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
         </span>
       </div>
     );
